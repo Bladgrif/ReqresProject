d1759589912aa1e3bd3bd0098268ca709cbb170e
Badgrif <aswd@asd.com>
2024-04-22 22:17:43 +0300

    Gross hack because Lyntor doesn't know how to code

--- src/test/java/com/reqres/api/tests/UserTests.java
+++ src/test/java/com/reqres/api/tests/UserTests.java
@@ -1,7 +1,11 @@
 package com.reqres.api.tests;
 
 import com.reqres.api.config.ApiConfig;
+import com.reqres.api.models.UserData;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.greaterThan;
@@ -18,5 +22,21 @@ public class UserTests extends ApiConfig {
                 .statusCode(200)
                 .assertThat()
                 .body("data",hasSize(greaterThan(0)));
+    }
+
+    @Test
+    void checkUserAndId() {
+        List<UserData> users= given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get("users?page=2")
+                .then()
+                .log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+
+        for (UserData u : users) {
+            System.out.println(u.getId() + " " + u.getAvatar());
+        }
+
     }
 }
